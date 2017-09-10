43e43505a2f648c9179cf1b3a2e229db3a31f731
Simon Addicott <simonaddicott@Simons-MacBook-Pro.local>
2017-09-10 18:53:59 +0100

    Added welcome page and first part of device registration request

--- app/src/main/java/simonaddicott/dev/freespeech/MainActivity.java
+++ app/src/main/java/simonaddicott/dev/freespeech/MainActivity.java
@@ -4,14 +4,22 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.provider.Settings;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -27,9 +35,15 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.data;
+
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences prefs = null;
+    EditText username;
+
+    String api = "http://api.freespeech.gq/data.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,24 +53,125 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        if (prefs.getBoolean("firstrun", true)) {
-            prefs.edit().putBoolean("firstrun", false).commit();
-            // First run
-            // todo
-            // display welcome
-            // obtain username
-            // internalKeyPair used for comms with server
-            KeyPair internalKeyPair = getKeyPair();
-            // externalKeyPair used for comms with contacts
-            KeyPair externalKeyPair = getKeyPair();
-            // register device
+        File id = new File("freespeech-id");
 
+        if (id.exists()) {
+
+            // display home page
 
         } else {
 
+            displayWelcomePage();
+
         }
+    }
+
+    public Boolean writeToFile(String fileName, String bodyToWrite) {
+        try {
+            FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos.write(bodyToWrite.getBytes());
+            fos.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public String readFromFile(String fileName) {
+        String returnContent = "";
+        try {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
+            String inputString;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((inputString = inputReader.readLine()) != null) {
+                stringBuffer.append(inputString);
+            }
+            returnContent = stringBuffer.toString();
+            return returnContent;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private void displayWelcomePage() {
+        // display welcome menu
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main);
+        layout.setBackgroundColor(getResources().getColor(R.color.black));
+
+        TextView head = new TextView(this);
+        head.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        head.setTextSize(getResources().getDimension(R.dimen.headTextSize));
+        head.setTextColor(getResources().getColor(R.color.white));
+        head.setText("Welcome");
+
+        TextView subHead = new TextView(this);
+        subHead.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        subHead.setTextSize(getResources().getDimension(R.dimen.subTextSize));
+        subHead.setTextColor(getResources().getColor(R.color.grey));
+        subHead.setText("Please enter a username to begin");
+
+        username = new EditText(this);
+
+        LinearLayout.LayoutParams usernameParam = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        usernameParam.setMargins(0, 100, 0, 100);
+        username.setLayoutParams(usernameParam);
+        username.setTextColor(getResources().getColor(R.color.white));
+        username.setHint("Username");
+        username.setHintTextColor(getResources().getColor(R.color.grey));
+
+        Button submit = new Button(this);
+        submit.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        submit.setBackgroundColor(getResources().getColor(R.color.darkGrey));
+        submit.setTextColor(getResources().getColor(R.color.white));
+        submit.setText("Go");
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().length() == 0) {
+                    // warn
+                } else {
+                    register(username.getText().toString());
+                }
+
+            }
+        });
+
+        layout.addView(head);
+        layout.addView(subHead);
+        layout.addView(username);
+        layout.addView(submit);
+    }
+
+    private boolean register(String username) {
+        // internalKeyPair used for comms with server
+        KeyPair internalKeyPair = getKeyPair();
+
+        // externalKeyPair used for comms with contacts
+        KeyPair externalKeyPair = getKeyPair();
+        // We'll use this later on
+
+        // Store the private key - using temp unsecure storage for now
+        writeToFile("self-serv-private", internalKeyPair.getPrivate().toString() ) ;
+
+        // Send of the request
+        RegisterRequest request = new RegisterRequest();
+        request.execute( username, internalKeyPair.getPublic().toString()) ;
+
+        return true;
     }
 
     public static KeyPair getKeyPair() {
@@ -72,42 +187,14 @@ public class MainActivity extends AppCompatActivity {
         return kp;
     }
 
-    public class HttpGetRequest extends AsyncTask<String, Void, String> {
+    public class RegisterRequest extends AsyncTask<String, Void, String> {
 
-        public Boolean writeToFile(String fileName, String bodyToWrite){
-            try {
-                FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
-                fos.write(bodyToWrite.getBytes());
-                fos.close();
-                return true;
-            } catch(IOException e){
-                return false;
-            }
-        }
 
-        public String readFromFile(String fileName){
-            String returnContent = "";
-            try {
-                BufferedReader inputReader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
-                String inputString;
-                StringBuffer stringBuffer = new StringBuffer();
-                while ((inputString = inputReader.readLine()) != null) {
-                    stringBuffer.append(inputString);
-                }
-                returnContent = stringBuffer.toString();
-                return returnContent;
-            } catch(IOException e){
-                return null;
-            }
-        }
-
-        private String getQuery(List<AbstractMap.SimpleEntry> params) throws UnsupportedEncodingException
-        {
+        private String getQuery(List<AbstractMap.SimpleEntry> params) throws UnsupportedEncodingException {
             StringBuilder result = new StringBuilder();
             boolean first = true;
 
-            for (AbstractMap.SimpleEntry pair : params)
-            {
+            for (AbstractMap.SimpleEntry pair : params) {
                 if (first)
                     first = false;
                 else
@@ -123,36 +210,40 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
-            String stringUrl = params[0];
+            String stringUrl = api;
             String result;
 
             try {
+
                 URL myUrl = new URL(stringUrl);
                 HttpURLConnection connection = (HttpURLConnection) myUrl.openConnection();
-                if( params[1] == "POST" ) {
-                    connection.setRequestMethod("POST");
-                    connection.setDoInput(true);
-                    connection.setDoOutput(true);
 
-                    String accountNumber = params[2];
-                    String passToken = params[3];
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
 
-                    String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+                String username = params[0];
+                String publicKey = params[1];
 
-                    List<AbstractMap.SimpleEntry> paramms = new ArrayList<AbstractMap.SimpleEntry>();
+                String android_id = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
-                    writeToFile("user", accountNumber);
+                List<AbstractMap.SimpleEntry> paramms = new ArrayList<AbstractMap.SimpleEntry>();
 
-                    OutputStream os = connection.getOutputStream();
+                paramms.add( new AbstractMap.SimpleEntry( "action", "register" ));
+                paramms.add( new AbstractMap.SimpleEntry( "username", username ) );
+                paramms.add( new AbstractMap.SimpleEntry( "public-key", publicKey ) );
+                paramms.add( new AbstractMap.SimpleEntry( "uuid", android_id ) );
+                paramms.add( new AbstractMap.SimpleEntry( "device", "android" ));
 
-                    BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "UTF-8") );
+                OutputStream os = connection.getOutputStream();
 
-                    writer.write(getQuery(paramms));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
-                    writer.flush();
-                    writer.close();
-                    os.close();
-                }
+                writer.write(getQuery(paramms));
+
+                writer.flush();
+                writer.close();
+                os.close();
 
                 //connection.setReadTimeout(6000);
                 //connection.setConnectTimeout(60);
@@ -165,18 +256,17 @@ public class MainActivity extends AppCompatActivity {
                 StringBuilder stringBuilder = new StringBuilder();
 
                 String line;
-                do{
+                do {
                     line = reader.readLine();
                     stringBuilder.append(line);
-                } while( line != null );
+                } while (line != null);
 
                 reader.close();
                 streamReader.close();
 
                 result = stringBuilder.toString();
                 return result;
-            }
-            catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
                 result = null;
             }
@@ -187,10 +277,28 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            if( result.contains("success") ){
+            if (result.length() > 1 ) {
                 try {
                     JSONObject jsonObject = new JSONObject(result);
-                } catch( JSONException e ){
+                    if( jsonObject.getString( "request" ) == "ok" ){
+
+                        Toast.makeText( getApplicationContext(), "Device registered", Toast.LENGTH_LONG );
+                        JSONObject data = jsonObject.getJSONObject(" data" );
+                        writeToFile( "freespeech-id", data.getString("new-id") );
+
+                        // load home page
+
+                    } else if( jsonObject.getString("request") == "failed" ){
+
+                        JSONObject data = jsonObject.getJSONObject(" data" );
+                        Toast.makeText( getApplicationContext(), data.getString("reason"), Toast.LENGTH_LONG );
+
+                    } else {
+
+                        Toast.makeText( getApplicationContext(), "UNKNOWN:" + result, Toast.LENGTH_LONG );
+
+                    }
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
